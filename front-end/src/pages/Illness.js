3bd0025758d3105836e34c3b07beddbb0c7e2697
Haroon Khan <hronkkhan6@gmail.com>
2023-12-18 18:25:22 -0800

    front-end setup

---
+++ front-end/src/pages/Illness.js
@@ -0,0 +1,8 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const Illness = (props) => {
+    return <h1>show page</h1>
+}
+
+export default Illness
