05d8cccf2ca19521e9b2eb038045db7a71a695d6
Haroon Khan <hronkkhan6@gmail.com>
2024-01-08 10:47:36 -0800

    final website

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div className="footer media">
-            <h1>Developers:</h1>
+        <div className="footer">
+            <h1>Developer Links:</h1>
             <Link to="https://www.linkedin.com/in/haroon-khan-0101z1010/"><h4>Haroon Khan</h4></Link>
             <Link to="https://www.linkedin.com/in/franciscruzjr/"><h4>Francis Cruz</h4></Link>
-            <Link to=""><h4>Anna Bilan</h4></Link>
+            <Link to="https://www.linkedin.com/in/bilananna/"><h4>Anna Bilan</h4></Link>
             <Link to="https://www.linkedin.com/in/mherrera491/"><h4>Matt Herrera</h4></Link> <br />
         </div>
     )
